1f64ea55a33c6f00114100baf9dff21724557f12
Your Name <you@example.com>
2017-07-06 11:10:23 -0400

    Initial commit of accelerometer code. STM32 nucleo L152RE base board

---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+//#include <stm32l1xx_gpio.h>
+//#include <stm32l1xx_rcc.h>
+#include <stm32l1xx_hal.h>
+
+#define LED_PIN                                GPIO_PIN_5
+#define LED_GPIO_PORT                          GPIOA
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+
+#endif // MAIN_H
