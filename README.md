3e74aeb31cb3d1e74c8de39b7fded76af6995d52
gnsensor <gnsensors@users.noreply.github.com>
2017-07-08 11:55:24 -0400

    Uart packet changed to more robust version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # iotNode
 
-This is a simple project to serially send accelerometer data over the serial port. 
+This is a simple project to serially send accelerometer data over the serial port.
 
 # Hardware
 
@@ -14,9 +14,15 @@ Uses the MBED platform as well as Platform.io running on Ubuntu 16.04 with Atom 
 
 
 # Usage
-The device will initialize the Accelerometer to sample at it fastest rate with an automatic BW setting.  It will store a buffer of 3 axis data (SAMPLE_SIZE) then send all samples over the serial port
+The device will initialize the Accelerometer to sample at it fastest rate with an automatic BW setting.  It will store a buffer of 3 axis 16 bit data (SAMPLE_SIZE* BYTES_PER_SAMPLE * NUM_OF_AXIS) then send all samples over the serial port.  A 32 bit CRC is added to the message
 
 ## Serial port settings
 115200 1N8 no flow control
 
-
+## Message format
+<HEADER><SYNC><LENGTH><PAYLOAD><CRC>
+HEADER = 0xAA 0xAA 0xAA 0xAA (this looks nice on an Oscope so it is easy to debug and see baudrate)
+SYNC = two bytes to show start of length 0xFF 0x00 (also nice on an oscope)
+LENGTH = 16 bit unsigned integer of the number of bytes being sent little Endian including CRC
+PAYLOAD = 8 bit Bytes of data
+CRC = CRC32 - 32 bit unsigned integer sent Little endian
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "mbed.h"
+#include "lib_crc.h"
+
 //#include "arm_math.h"
 
-
-
 #define ID_REG (0x0F) // ID Register
-
 #define ACC_ADR_W     (0xD6)
 #define ACC_ADR_R     (0xD7)
 #define SAMPLE_SIZE     (512)
+#define BYTES_PER_SAMPLE  (2)
+#define NUM_OF_AXIS     (3)
+#define BUFFER_SIZE     SAMPLE_SIZE * BYTES_PER_SAMPLE * NUM_OF_AXIS
+#define CRC_SIZE    (4) //number of bytes for CRC32
 #define ACC_NEW_DATA_EVNT 0x00000001
-
 
 I2C i2c(I2C_SDA, I2C_SCL);
 
@@ -19,16 +21,7 @@ InterruptIn _Interrup(D4);
 
 Serial pc(SERIAL_TX, SERIAL_RX);
 
-
-typedef struct _vector{
-
-    int16_t x;
-    int16_t y;
-    int16_t z;
-} vector_t;
-
 volatile uint32_t e =0;
-
 
 int ACC_Config(uint8_t reg, uint8_t val)
 {
@@ -62,27 +55,26 @@ uint8_t ACC_Read_Status(void)
 }
 
 
-int ACC_Read_Axis(int16_t* x, int16_t* y, int16_t* z)
+int ACC_Read_Axis(uint8_t* buffer)
 {
 
-    char data[6];
+    char localBuff[6];
     char cmd;
+    int i;
 
-
-
-      cmd = 0x28;
+    cmd = 0x28;
 
     int status = i2c.write(ACC_ADR_W, &cmd, 1);
     if (status != 0) { // Error
         pc.printf("ERROR I2C\n");
     }
 
-    i2c.read(ACC_ADR_R, data, 6);
+    i2c.read(ACC_ADR_R, localBuff, 6);
 
-    *x = (int16_t) (data[0] | (data[1]>>8 & 0xFF00));
-    *y = (int16_t) (data[2] | (data[3]>>8 & 0xFF00));
-    *z = (int16_t) (data[4] | (data[5]>>8 & 0xFF00));
-
+    for(i=0;i<6; i++)
+    {
+      buffer[i] = localBuff[i];
+    }
 
     return  status;
 
@@ -98,24 +90,24 @@ void isr(void)
 int main()
 {
 
-    vector_t v[SAMPLE_SIZE];
+    uint8_t sampleBuffer[BUFFER_SIZE];
     int32_t index = 0;
-
     uint8_t intStatus;
+    uint32_t crc;
+    uint8_t buff[6];
+    uint32_t i;
 
-    int16_t x, y, z;
+    uint16_t txBufferSize = BUFFER_SIZE + CRC_SIZE;
 
-    x = 0;
-    y=0;
-    z=0;
+    uint8_t lengthLSB =(uint8_t) (txBufferSize & 0x00FF);
+    uint8_t lengthMSB = (uint8_t) ((txBufferSize>>8) & 0x00FF);
 
-     pc.baud (115200);
-     i2c.frequency(400000);
+    pc.baud (115200);
+    i2c.frequency(400000);
 
      _Interrup.fall(&isr);
 
 		 myled = 0;
-
 
     pc.printf("Starting\n");
 
@@ -129,14 +121,14 @@ int main()
 
 		// read the status and axis data to clear interrupts
   	intStatus = ACC_Read_Status();
-		ACC_Read_Axis(&x, &y, &z);
+
+		ACC_Read_Axis(buff);
 
     while(1)
     {
 
         if((e & ACC_NEW_DATA_EVNT)>0)
         {
-
             //clear event
             __disable_irq();
             e &= (~ACC_NEW_DATA_EVNT);
@@ -146,24 +138,49 @@ int main()
             intStatus = ACC_Read_Status();
             if((intStatus & 0x01)>0)
             {
-                ACC_Read_Axis(&x, &y, &z);
+                ACC_Read_Axis(buff);
 
-                if(index<SAMPLE_SIZE)
+                if(index<(BUFFER_SIZE))
                 {
-                    v[index].x = x;
-                    v[index].y = y;
-                    v[index].z = z;
+                  //Copy new samples into buffer
+                  for(i=0;i<6;i++)
+                  {
+                    sampleBuffer[index] = buff[i];
                     index++;
+                  }
                 }
                 else
                 {
                     index = 0;
-                    pc.printf("AAAA\n");
-                    for(int i=0;i<SAMPLE_SIZE; i++)
+                    crc = calculate_crc32((char*) sampleBuffer,BUFFER_SIZE);
+
+                    //<HEADER>
+                    pc.putc(0xAA);
+                    pc.putc(0xAA);
+                    pc.putc(0xAA);
+                    pc.putc(0xAA);
+
+                    //SYNC
+                    pc.putc(0xFF);
+                    pc.putc(0x00);
+
+
+                    //<LENGTH>
+                    pc.putc(lengthLSB);
+                    pc.putc(lengthMSB);
+
+                    //<PAYLOAD>
+                    for(int i=0;i<BUFFER_SIZE; i++)
                     {
-                        pc.printf("%d, %d, %d\n", v[i].x, v[i].y, v[i].z);
+                        pc.putc(sampleBuffer[i]);
                     }
-                    pc.printf("BBBB\n");
+
+                    //<CRC>
+                    pc.putc(crc & 0x000000FF);
+                    pc.putc((crc>>8) & 0x000000FF);
+                    pc.putc((crc>>16) & 0x000000FF);
+                    pc.putc((crc>>24) & 0x000000FF);
+
                 }
             }
         }
